2b3e029c29de0caca58823e365374dd5b88caa72
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 17:02:41 +0300

    Rokas

--- script.js
+++ script.js
@@ -6,8 +6,35 @@ const slideshowDivs = () => {
 
         div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg`
 
+        i == 1 && div.classList.add('change')
+
         document.querySelector('.slideshow').appendChild(div)
     }
 }
 
 slideshowDivs();
+
+const divs = document.querySelectorAll('.slideshow div')
+
+let a = 1;
+
+const slideshow = () => {
+    setInterval(() => {
+
+        a++
+
+        const div = document.querySelector('.slideshow .change')
+
+        div.classList.remove('change')
+
+        if(a < divs.length){
+            div.nextElementSibling.classList.add('change')
+        } else {
+            divs[0].classList.add('change')
+            a = 1
+        }
+
+    }, 20000)
+}
+
+slideshow();
